fac03f1ef3491d7ecf0d36d25aedb4da641c4a89
pringon <gojedan98@yahoo.com>
2019-02-24 15:46:11 +0000

    Remove filler files and try out react-charts

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+import { Chart } from 'react-charts';
+
+import { mockData } from './mockData';
+
+interface props {};
+
+interface state {
+  data: number[][]; 
+};
+
+class App extends Component<props, state> {
+
+  constructor(props: props) {
+    super(props);
+
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      data: mockData.map((dataPoint: number, index: number) => [index * 0.5, dataPoint]),
+    });
+  }
+
   render() {
+    console.log(this.state.data);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App"
+         style={{
+            width: "400px",
+            height: "300px"
+          }}
+      >
+        <Chart
+          data={[
+            {
+              label: 'Voltage change over time',
+              data: this.state.data,
+            }
+          ]}
+          axes={[
+            { primary: true, type: "linear", position: "bottom" },
+            { type: "linear", position: "left" }
+          ]}
+        />
       </div>
     );
   }
---
+++ src/mockData.ts
@@ -0,0 +1,14 @@
+export const mockData: number[] = [
+  2.3,
+  2.5,
+  2.6,
+  3.1,
+  5.3,
+  2.1,
+  1.9,
+  2.3,
+];
+
+export default {
+  mockData,
+};
